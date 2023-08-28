88b6b5d8d29ae419a24f0bf4305222d5128643ef
Sam Ruwe <ruwesam@gmail.com>
2023-08-28 14:55:15 -0400

    chore: Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,21 @@
 plugins {
-    id("java")
+    java
     id("org.shipkit.shipkit-auto-version") version "1.2.2"
     id("org.shipkit.shipkit-changelog") version "1.2.0"
 }
 
+tasks.generateChangelog {
+    repository = "SamuelRuwe/github-actions"
+    previousRevision = "0.0.1"
+    githubToken = System.getenv("GITHUB_TOKEN")
+}
+
 group = "org.puregeniusness"
 version = "1.0-SNAPSHOT"
+
+////"org.shipkit.shipkit-changelog" {
+//
+//}
 
 repositories {
     mavenCentral()
@@ -20,8 +30,10 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.named("generateChangelog") {
-//    val previousRevision = project.ext["shipkit-auto-version-previous-tag"]
+//tasks.named("generateChangelog") {
+//    project.setProperty("repository", "SamuelRuwe/github-actions")
+//        SamuelRuwe/github-actions
+//    val previousRevision = project.ext.get("shipkit-auto-version-previous-tag")
 //    println(previousRevision)
-    println("Success")
-}
+//    println("Success")
+//}
