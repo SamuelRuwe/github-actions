37b6f8ed5fedbc8d64b6df73b1e74c365b6a8917
Sam Ruwe <ruwesam@gmail.com>
2023-08-31 09:37:19 -0400

    Add committer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,58 @@
 plugins {
     java
-    id("org.shipkit.shipkit-auto-version") version "1.2.2"
-    id("org.shipkit.shipkit-changelog") version "1.2.0"
+    id("se.bjurr.gitchangelog.git-changelog-gradle-plugin") version("+")
 }
 
-tasks.generateChangelog {
-    repository = "SamuelRuwe/github-actions"
-    previousRevision = "0.0.1"
-    githubToken = System.getenv("GITHUB_TOKEN")
-}
+//tasks.generateChangelog {
+//    repository = "SamuelRuwe/github-actions"
+//    previousRevision = "0.0.1"
+//    githubToken = System.getenv("GITHUB_TOKEN")
+//}
 
 group = "org.puregeniusness"
 version = "1.0-SNAPSHOT"
 
-////"org.shipkit.shipkit-changelog" {
-//
-//}
 
+// Optional config if you want to configure the changelog
+tasks.register<se.bjurr.gitchangelog.plugin.gradle.GitChangelogTask>("generateGitChangelog") {
+    templateContent = """
+{{#tags}}
+## {{name}}
+ {{#issues}}
+  {{#hasissue}}
+   {{#haslink}}
+### {{name}} [{{issue}}]({{link}}) {{title}} {{#hasissuetype}} *{{issuetype}}* {{/hasissuetype}} {{#haslabels}} {{#labels}} *{{.}}* {{/labels}} {{/haslabels}}
+   {{/haslink}}
+   {{^haslink}}
+### {{name}} {{issue}} {{title}} {{#hasissuetype}} *{{issuetype}}* {{/hasissuetype}} {{#haslabels}} {{#labels}} *{{.}}* {{/labels}} {{/haslabels}}
+   {{/haslink}}
+  {{/hasissue}}
+  {{^hasissue}}
+### {{name}}
+  {{/hasissue}}
+
+  {{#commits}}
+**{{{messagetitle}}}**
+
+{{#messagebodyitems}}
+ * {{.}}
+{{/messagebodyitems}}
+
+[{{hash}}](https://github.com/{{ownername}}/{{reponame}}/commit/{{hash}}) {{authorname}} *{{committime}}*
+
+  {{/commits}}
+
+ {{/issues}}
+{{/tags}}
+ """.trimIndent()
+}
+
+//task gitChangelogVersionTask(type: se.bjurr.gitchangelog.plugin.gradle.GitChangelogSemanticVersionTask) {
+//    suffixSnapshot = true;
+//    majorVersionPattern = "^[Bb]reaking"
+//    minorVersionPattern = "[Ff]eature"
+//    patchVersionPattern = "[Ff]ix"
+//}
 repositories {
     mavenCentral()
 }
